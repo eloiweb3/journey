34342c6e13a8eb16aa2b6dae7e04d14b49beaba9
Eloi <recycled_loops@hotmail.com>
2025-05-08 14:25:40 +0200

    add Anchor instructions

---
+++ anchor/programs/journey/src/lib.rs
@@ -0,0 +1,74 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("GiT5e2Vg9yTqiozmxjJG86ZLaejUstRTKbkDSz6JSLDk");
+
+#[program]
+pub mod journey {
+    use super::*;
+
+    pub fn create_journey_entry(
+        ctx: Context<CreateJourneyEntry>,
+        title: String,
+        message: String,
+    ) -> Result<()> {
+        let journey_entry = &mut ctx.accounts.journey_entry;
+        journey_entry.owner = *ctx.accounts.owner.key;
+        journey_entry.title = title;
+        journey_entry.message = message;
+        Ok(())
+    }
+
+    pub fn update_journey_entry(
+        ctx: Context<UpdateJourneyEntry>,
+        _title: String,
+        message: String,
+    ) -> Result<()> {
+        let journey_entry = &mut ctx.accounts.journey_entry;
+        journey_entry.message = message;
+        Ok(())
+    }
+
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct CreateJourneyEntry<'info> {
+    #[account(init,
+      seeds = [title.as_bytes(), owner.key().as_ref()],
+      bump,
+      space = 8 + JourneyEntryState::INIT_SPACE,
+      payer = owner)]
+    pub journey_entry: Account<'info, JourneyEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateJourneyEntry<'info> {
+    #[account(
+      mut,
+      seeds = [title.as_bytes(), owner.key().as_ref()],
+      bump,
+      realloc = 8 + JourneyEntryState::INIT_SPACE,
+      realloc::payer = owner,
+      realloc::zero = true,
+  )]
+    pub journey_entry: Account<'info, JourneyEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct JourneyEntryState {
+  pub owner: Pubkey,
+  #[max_len(50)]
+  pub title: String,
+  #[max_len(1000)]
+  pub message: String,
+}
